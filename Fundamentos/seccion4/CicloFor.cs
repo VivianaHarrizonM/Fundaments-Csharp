f0356061cfa5c679c4d57e95ef1642b08680ba49
Viviana Harrizon <viviana.harrizon@gmail.com>
2025-05-09 17:11:55 -0600

    Add seccion4

--- Fundamentos/seccion4/CicloFor.cs
+++ Fundamentos/seccion4/CicloFor.cs
@@ -7,11 +7,15 @@ using System.Threading.Tasks;
 namespace Fundamentos.seccion4
 {
     internal class CicloFor
-    {
+    {/*
         static void Main(string[] args)
         {
-            double calif1, calif2, calif3, promedio;
-
+            double calif1, calif2, calif3, calificacion;
+            int numCalif;
+            double promedio = 0;
+            int acumulador = 0;
+            int precio;
+            /*
             Console.WriteLine("Dame tu primer calificación");
             calif1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Dame tu segunda calificación");
@@ -21,7 +25,39 @@ namespace Fundamentos.seccion4
 
             promedio = (calif1 + calif2 + calif3) / 3;
 
+
             Console.WriteLine("El promedio es: {0}", promedio);
+            */
+        /*
+            Console.WriteLine("*********** Promedio  **************");
+            Console.WriteLine(".");
+            Console.Write("¿Cuantas calificaciones vas a evaluar? ");
+            
+            numCalif = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= numCalif; i++)
+            {
+                Console.Write($"Dame tu calificación {i} : ");
+                calificacion = Convert.ToDouble(Console.ReadLine());
+                promedio += calificacion; 
+            }
+            promedio /= numCalif; 
+            Console.Write("El promedio es: {0} ", promedio);
+
+            
+            Console.WriteLine("/n******* Caja rejistradora ***********");
+           
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write($"Ingrresa el precio del producto {i} : ");
+                precio = Convert.ToInt32(Console.ReadLine());
+                acumulador += precio;
+            }
+
+            Console.Write($"El total es {acumulador} : ");
         }
+
+       */
+
     }
 }
