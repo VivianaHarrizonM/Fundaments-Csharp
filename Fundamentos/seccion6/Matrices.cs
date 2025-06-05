b953b2dd192bdf1853853dbf0f1a19e5e7bf3df4
Viviana Harrizon <viviana.harrizon@gmail.com>
2025-06-04 20:09:23 -0600

    add seccion 6.1

--- Fundamentos/seccion6/Matrices.cs
+++ Fundamentos/seccion6/Matrices.cs
@@ -10,11 +10,25 @@ namespace Fundamentos.seccion6
     {
         static void Main(string[] args)
         {
-            //tipo[] nombre = new tipo[tamaño];
-            double[] calificaciones = new double[4]; //Declaracion de una matriz de tipo "double
-            String[] nombres = new String[5];
-            int[] numeros = new int[10];
-            byte[] datos = new byte[7];
+            //byte tamaño;
+            //Console.Write(" ¿De qué tamaño será la matriz? ");
+            //tamaño = Convert.ToByte(Console.ReadLine());
+
+            //int[] primos = new int[tamaño];
+
+            ////tipo[] nombre = new tipo[tamaño];
+            //double[] calificaciones = new double[4]; //Declaracion de una matriz de tipo "double
+            //String[] nombres = new String[5];
+            //int[] numeros = new int[10];
+            //byte[] datos = new byte[7];
+
+            double[] calificaciones = new double[4] { 9, 10, 8.9, 9.2};
+            int[] numeros = { 1,2,4};
+            int[] num;
+            num = new int[] { 1,3,4};
+            string[] nombres = new string[4] { "Sandra", "Ariel", "Joel", "Laura" };
+
+
         }
     }
 }
