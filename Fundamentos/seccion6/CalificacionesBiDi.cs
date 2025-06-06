506cf823b45c4a6686f14f020c4009c65dc93a20
Viviana Harrizon <viviana.harrizon@gmail.com>
2025-06-05 18:52:39 -0600

    calificaciones

---
+++ Fundamentos/seccion6/CalificacionesBiDi.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos.seccion6
+{
+    internal class CalificacionesBiDi
+    {
+        static void Main(string[] args)
+        {
+                    byte i,j, numCal, salones;
+                    double sumaCalif = 0, promedio, califMin = 10, califMax = 0;
+
+                    Console.Write("Ingresa el numero de calificaciones: ");
+                    numCal = Convert.ToByte(Console.ReadLine());
+
+                    double[] calificaciones = new double[numCal];
+
+                    for (i = 0; i < numCal; i++)
+                    {
+                        Console.Write("Ingresa la primer calificación: ");
+                        calificaciones[i] = Convert.ToDouble(Console.ReadLine());
+
+                        sumaCalif += calificaciones[i];
+                    }
+
+
+                    promedio = sumaCalif / numCal;
+
+
+                    for (i = 0; i < numCal; i++)
+                    {
+                        if (calificaciones[i] < califMin)
+                        {
+                            califMin = calificaciones[i];
+                        }
+
+                    }
+                    for (i = 0; i < numCal; i++)
+                    {
+                        if (calificaciones[i] > califMax)
+                        {
+                            califMax = calificaciones[i];
+                        }
+
+                    }
+                    Console.WriteLine("\nEl promedio es {0}: ", promedio);
+                    Console.WriteLine("\nLa calificación maxima es {0}: ", califMax);
+                    Console.WriteLine("\nLa calificaión minima es {0}: ", califMin);
+        }
+            
+    }
+}
--- Fundamentos/seccion6/Matrices.cs
+++ Fundamentos/seccion6/Matrices.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Fundamentos.seccion6
 {
     internal class Matrices
-    {
+    {/*
         static void Main(string[] args)
         {
             //byte tamaño;
@@ -21,14 +21,44 @@ namespace Fundamentos.seccion6
             //String[] nombres = new String[5];
             //int[] numeros = new int[10];
             //byte[] datos = new byte[7];
+            //double[] calificaciones = new double[4] { 9, 10, 8.9, 9.2};
+            //int[] numeros = { 1,2,4};
+            //int[] num;
+            //num = new int[] { 1,3,4};
+            //string[] nombres = new string[4] { "Sandra", "Ariel", "Joel", "Laura" };
 
-            double[] calificaciones = new double[4] { 9, 10, 8.9, 9.2};
-            int[] numeros = { 1,2,4};
-            int[] num;
-            num = new int[] { 1,3,4};
-            string[] nombres = new string[4] { "Sandra", "Ariel", "Joel", "Laura" };
+            //double[] calificaciones = new double[4];
+            //calificaciones[0] = 9;
+            //calificaciones[1] = 10;
+            //calificaciones[2] = 7.4;
+            //calificaciones[3] = 6;
+            //Console.WriteLine(calificaciones[2]);
 
+            //double califFinal;
+            //califFinal = calificaciones[2]-1;
+            //Console.WriteLine(califFinal);
+
+
+            string[] nombres = new string[3];
+
+            //nombres[0] = "Noe";
+            //nombres[1] = "Joi";
+            //nombres[2] = "lua";
+
+            byte i;
+
+            for (i = 0; i < nombres.Length; i++)
+            {
+                Console.Write("Ingresa el valor para el indice {0}: ", i);
+                nombres[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nValores ingresados:");
+            for (i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine(nombres[i]);
+            }
 
         }
-    }
+ */   }
 }
