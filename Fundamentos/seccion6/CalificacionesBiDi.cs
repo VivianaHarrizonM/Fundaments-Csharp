309f922f11a30b096d473dd357418823e0b9d21b
Viviana Harrizon <viviana.harrizon@gmail.com>
2025-06-06 20:10:25 -0600

    Add  matriz escalonada

--- Fundamentos/seccion6/CalificacionesBiDi.cs
+++ Fundamentos/seccion6/CalificacionesBiDi.cs
@@ -8,48 +8,44 @@ namespace Fundamentos.seccion6
 {
     internal class CalificacionesBiDi
     {
-        static void Main(string[] args)
+       /* static void Main(string[] args)
         {
-                    byte i,j, numCal, salones;
-                    double sumaCalif = 0, promedio, califMin = 10, califMax = 0;
+            byte i, j, numCal, alumnos;
+            double sumaCalif = 0, promedio, califMin = 10, califMax = 0;
 
-                    Console.Write("Ingresa el numero de calificaciones: ");
-                    numCal = Convert.ToByte(Console.ReadLine());
+            Console.Write("Ingresa el número de calificaciones por alumno: ");
+            numCal = Convert.ToByte(Console.ReadLine());
 
-                    double[] calificaciones = new double[numCal];
+            Console.Write("Ingresa el número de alumnos: ");
+            alumnos = Convert.ToByte(Console.ReadLine());
 
-                    for (i = 0; i < numCal; i++)
-                    {
-                        Console.Write("Ingresa la primer calificación: ");
-                        calificaciones[i] = Convert.ToDouble(Console.ReadLine());
+            double[,] calificaciones = new double[alumnos, numCal];
 
-                        sumaCalif += calificaciones[i];
-                    }
+            // Ingreso de calificaciones
+            for (i = 0; i < alumnos; i++)
+            {
+                Console.WriteLine($"\nAlumno {i + 1}:");
+                for (j = 0; j < numCal; j++)
+                {
+                    Console.Write($"  Ingresa la calificación #{j + 1}: ");
+                    double calif = Convert.ToDouble(Console.ReadLine());
 
+                    calificaciones[i, j] = calif;
+                    sumaCalif += calif;
 
-                    promedio = sumaCalif / numCal;
+                    if (calif < califMin) califMin = calif;
+                    if (calif > califMax) califMax = calif;
+                }
+            }
 
+            promedio = sumaCalif / (alumnos * numCal);
 
-                    for (i = 0; i < numCal; i++)
-                    {
-                        if (calificaciones[i] < califMin)
-                        {
-                            califMin = calificaciones[i];
-                        }
-
-                    }
-                    for (i = 0; i < numCal; i++)
-                    {
-                        if (calificaciones[i] > califMax)
-                        {
-                            califMax = calificaciones[i];
-                        }
-
-                    }
-                    Console.WriteLine("\nEl promedio es {0}: ", promedio);
-                    Console.WriteLine("\nLa calificación maxima es {0}: ", califMax);
-                    Console.WriteLine("\nLa calificaión minima es {0}: ", califMin);
+            // Resultados
+            Console.WriteLine("\n--- Resultados ---");
+            Console.WriteLine($"Promedio general: {promedio:F2}");
+            Console.WriteLine($"Calificación máxima: {califMax}");
+            Console.WriteLine($"Calificación mínima: {califMin}");
         }
-            
+       */
     }
 }
---
+++ Fundamentos/seccion6/MatrizEscalonada.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos.seccion6
+{
+    internal class MatrizEscalonada
+    {
+        static void Main(string[] args)
+        {
+            //int[] numeros = new int[4] { 23, 13, 5, 6 };
+
+            //Console.WriteLine(numeros.Length);
+            //double[,] ventas = new double[4, 3]
+            //    {
+            //        { 155, 100, 170 },
+            //        { 206, 120 , 130},
+            //        { 115, 190, 104 },
+            //        { 163, 218, 125 }
+
+            //    };
+            //Console.WriteLine(ventas.Length);
+
+            //double[][] ventas = new double[4][];
+
+            //ventas[0] = new double[3] { 155, 100, 170};
+            //ventas[1] = new double[2] { 206, 120 };
+            //ventas[2] = new double[4] { 115,190, 104, 130};
+            //ventas[3] = new double[3] { 163, 218,125};
+
+            double[][] ventas = new double[4][]
+                {
+                    new double[3] { 155, 100, 170 },
+                    new double[2] { 206, 120 },
+                    new double[4] { 115, 190, 104, 130 },
+                    new double[3] { 163, 218, 125 }
+
+                };
+            Console.WriteLine(ventas.Length);
+
+
+        }
+    }
+}
