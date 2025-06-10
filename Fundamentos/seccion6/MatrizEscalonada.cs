3fda3efb5f9d74c267e72ed9bacd582927928f88
Viviana Harrizon <viviana.harrizon@gmail.com>
2025-06-09 20:00:36 -0600

    Changes in Matriz Escalonada

--- Fundamentos/seccion6/MatrizEscalonada.cs
+++ Fundamentos/seccion6/MatrizEscalonada.cs
@@ -29,16 +29,24 @@ namespace Fundamentos.seccion6
             //ventas[1] = new double[2] { 206, 120 };
             //ventas[2] = new double[4] { 115,190, 104, 130};
             //ventas[3] = new double[3] { 163, 218,125};
-
+            int i;
+            int j;
             double[][] ventas = new double[4][]
                 {
-                    new double[3] { 155, 100, 170 },
-                    new double[2] { 206, 120 },
-                    new double[4] { 115, 190, 104, 130 },
-                    new double[3] { 163, 218, 125 }
+                    new double[] { 155, 100, 170 },
+                    new double[] { 206, 120 },
+                    new double[] { 115, 190, 104, 130 },
+                    new double[] { 163, 218, 125 }
 
                 };
-            Console.WriteLine(ventas.Length);
+            for (i = 0; i < ventas.Length; i++)
+            {
+                Console.WriteLine("Elemento: {0}", i);
+                for (j = 0; j < ventas[i].Length; j++)
+                {
+                    Console.WriteLine(ventas[i][j]);
+                }
+            }
 
 
         }
